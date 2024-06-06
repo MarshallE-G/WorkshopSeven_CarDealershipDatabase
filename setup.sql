28605316918970d65b41ed285706c8ab4980cccb
Marshall <mellisgibbs@gmail.com>
2024-06-06 16:10:36 -0400

    created 'dealership_db' Database and created 'Dealerships' table

---
+++ setup.sql
@@ -0,0 +1,24 @@
+# ---------------------------------------------------------------------- #
+# Target DBMS:           MySQL                                           #
+# Project name:          Dealership Database                             #
+# ---------------------------------------------------------------------- #
+DROP DATABASE IF EXISTS dealership_db;
+
+CREATE DATABASE IF NOT EXISTS dealership_db;
+
+USE dealership_db;
+
+# ---------------------------------------------------------------------- #
+# Tables                                                                 #
+# ---------------------------------------------------------------------- #
+# ---------------------------------------------------------------------- #
+# Add table "Dealerships"                                                #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE Dealerships (
+	Dealership_id int NOT NULL auto_INCREMENT,
+	Name varchar(50) NOT NULL,
+	Address varchar(50),
+	Phone varchar(12),
+    PRIMARY KEY (dealership_id)
+ );
