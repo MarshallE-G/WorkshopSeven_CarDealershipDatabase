73b08fcb8945b58da2419e306c903d928e5b6d83
Marshall <mellisgibbs@gmail.com>
2024-06-09 17:59:47 -0400

    (WIP) adjusting query 6

--- queries.sql
+++ queries.sql
@@ -39,9 +39,20 @@
 -- 													AND Model_Name LIKE 'Explorer'));
 
 -- Query 6
-SELECT VIN
-	FROM Inventory
-    WHERE Dealership_id = 1;
+SELECT d.Dealership_id, s.*
+	FROM Sales_Contracts AS s
+    JOIN Dealerships AS d
+		ON d.Dealership_id IN (SELECT Dealership_id
+								FROM Inventory
+								WHERE VIN IN (SELECT VIN
+												FROM Sales_Contracts)
+								OR VIN IN (SELECT VIN
+											FROM Lease_Contracts));
+		
+
+-- SELECT VIN
+-- 	FROM Inventory
+--     WHERE Dealership_id = 1;
 
 -- SELECT Dealership_id
 -- 	FROM Inventory
