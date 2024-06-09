bda8e38efe4e240517aa7c7ced72d0b093dacd83
Marshall <mellisgibbs@gmail.com>
2024-06-08 22:56:34 -0400

    completed second query

--- queries.sql
+++ queries.sql
@@ -1,1 +1,7 @@
-SELECT * FROM Dealerships;
+-- SELECT * FROM Dealerships;
+
+SELECT i.Dealership_id, v.*
+	FROM Vehicles AS v
+    JOIN Inventory As i
+		ON v.VIN = i.VIN
+    WHERE Dealership_id = 1;
