c0cd96550c3d108c958569952a6899a51859bcc4
Marshall <mellisgibbs@gmail.com>
2024-06-09 16:44:11 -0400

    made an alternative way to accomplish query 4

--- queries.sql
+++ queries.sql
@@ -1,17 +1,28 @@
+-- Query 1
 -- SELECT * FROM Dealerships;
 
+-- Query 2
 -- SELECT i.Dealership_id, v.*
 -- 	FROM Vehicles AS v
 --     JOIN Inventory As i
 -- 		ON v.VIN = i.VIN
 --     WHERE Dealership_id = 1;
 
+-- Query 3
 -- SELECT *
 -- 	FROM Vehicles
 --     WHERE VIN = 12345;
 
-SELECT i.VIN, d.*
-	FROM Dealerships AS d
-    JOIN Inventory AS i
-		ON d.Dealership_id = i.Dealership_id
-	WHERE i.VIN = 12345;
+-- Query 4
+-- SELECT i.VIN, d.*
+-- 	FROM Dealerships AS d
+--     JOIN Inventory AS i
+-- 		ON d.Dealership_id = i.Dealership_id
+-- 	WHERE i.VIN = 12345;
+
+-- Alternative to query 4
+SELECT *
+	FROM Dealerships
+    WHERE Dealership_id IN (SELECT Dealership_id
+								FROM Inventory
+								WHERE VIN = 12345);
