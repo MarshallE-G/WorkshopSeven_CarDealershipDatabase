af5eb5a882e2a0fd6bbe710dc2d858294679efb5
Marshall <mellisgibbs@gmail.com>
2024-06-08 22:30:45 -0400

    fixed issues and added comments

--- setup.sql
+++ setup.sql
@@ -82,17 +82,29 @@ CREATE TABLE Lease_Contracts (
     PRIMARY KEY (Lease_id)
 );
 
+# ---------------------------------------------------------------------- #
+# Add info into "Dealerships"                                            #
+# ---------------------------------------------------------------------- #
+
 TRUNCATE TABLE Dealerships;
 INSERT INTO Dealerships	VALUES (null, 'D & B Used Cars', '111 Old Benbrook Rd', '817-555-5555');
 
+# ---------------------------------------------------------------------- #
+# Add info into "Vehicles"                                               #
+# ---------------------------------------------------------------------- #
+
 TRUNCATE TABLE Vehicles;
-INSERT INTO Vehicles VALUES	(null, 10112, 1993, 'Ford', 'Explorer', 'SUV', 'Red', 525123, 995.00, 'NO');
-INSERT INTO Vehicles VALUES	(null, 37846, 2001, 'Ford', 'Ranger', 'truck', 'Yellow', 172544, 1995.00, 'NO');
-INSERT INTO Vehicles VALUES	(null, 44901, 2012, 'Honda', 'Civic', 'SUV', 'Gray', 103221, 6995.00, 'NO');
-INSERT INTO Vehicles VALUES	(null, 72105, 2013, 'Chevrolet', 'Malibu', 'Sedan', 'Blue', 98345, 7995.00, 'NO');
-INSERT INTO Vehicles VALUES	(null, 78901, 2014, 'BMW', '328i', 'Sedan', 'Black', 90567, 10500.00, 'YES');
-INSERT INTO Vehicles VALUES	(null, 12345, 2010, 'Ford', 'Focus', 'Sedan', 'Green', 120345, 5500.00, 'YES');
-INSERT INTO Vehicles VALUES	(null, 89012, 2012, 'Mercedes-Benz', 'C-Class', 'Sedan', 'White', 112987, 9500.00, 'YES');
+INSERT INTO Vehicles VALUES	(10112, 1993, 'Ford', 'Explorer', 'SUV', 'Red', 525123, 995.00, 'NO');
+INSERT INTO Vehicles VALUES	(37846, 2001, 'Ford', 'Ranger', 'truck', 'Yellow', 172544, 1995.00, 'NO');
+INSERT INTO Vehicles VALUES	(44901, 2012, 'Honda', 'Civic', 'SUV', 'Gray', 103221, 6995.00, 'NO');
+INSERT INTO Vehicles VALUES	(72105, 2013, 'Chevrolet', 'Malibu', 'Sedan', 'Blue', 98345, 7995.00, 'NO');
+INSERT INTO Vehicles VALUES	(78901, 2014, 'BMW', '328i', 'Sedan', 'Black', 90567, 10500.00, 'YES');
+INSERT INTO Vehicles VALUES	(12345, 2010, 'Ford', 'Focus', 'Sedan', 'Green', 120345, 5500.00, 'YES');
+INSERT INTO Vehicles VALUES	(89012, 2012, 'Mercedes-Benz', 'C-Class', 'Sedan', 'White', 112987, 9500.00, 'YES');
+
+# ---------------------------------------------------------------------- #
+# Add info into "Inventory"                                              #
+# ---------------------------------------------------------------------- #
 
 TRUNCATE TABLE Inventory;
 INSERT INTO Inventory VALUES (1, 10112);
@@ -103,9 +115,17 @@ INSERT INTO Inventory VALUES (1, 78901);
 INSERT INTO Inventory VALUES (1, 12345);
 INSERT INTO Inventory VALUES (1, 89012);
 
+# ---------------------------------------------------------------------- #
+# Add info into "Sales_Contracts"                                        #
+# ---------------------------------------------------------------------- #
+
 TRUNCATE Sales_Contracts;
 INSERT INTO Sales_Contracts VALUES (null, 20240518, 'Mars Hall', 'mhall@aol.com', 78901, 'Financed', 11620.00, 493.85);
 INSERT INTO Sales_Contracts VALUES (null, 20240608, 'Johnny Boy', 'jboy@outlook.com', 89012, 'Not Financed', 10370.00, 0.00);
+
+# ---------------------------------------------------------------------- #
+# Add info into "Lease_Contracts"                                        #
+# ---------------------------------------------------------------------- #
 
 TRUNCATE Lease_Contracts;
 INSERT INTO Lease_Contracts VALUES (null, 20240518, 'Mars Hall', 'mhall@aol.com', 12345, 2750.00, 3135.00, 125.40);
