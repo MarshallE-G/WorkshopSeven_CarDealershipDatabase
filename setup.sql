6499feceae77bb96db5924241f59aa6530325071
Marshall <mellisgibbs@gmail.com>
2024-06-14 14:46:18 -0400

    added more sample data to 'vehicles' and 'inventory' tables

--- setup.sql
+++ setup.sql
@@ -109,6 +109,29 @@ INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `
 INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES	(78901, 2014, 'BMW', '328i', 'Sedan', 'Black', 90567, 10500.00, 'YES');
 INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES	(12345, 2010, 'Ford', 'Focus', 'Sedan', 'Green', 120345, 5500.00, 'YES');
 INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES	(89012, 2012, 'Mercedes-Benz', 'C-Class', 'Sedan', 'White', 112987, 9500.00, 'YES');
+-- Below was converted using ChatGPT
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (21245, 2015, 'Toyota', 'RAV4', 'SUV', 'Silver', 12000, 12000.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (22356, 2011, 'Kia', 'Optima', 'Sedan', 'Black', 109876, 6700.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (23467, 2013, 'Chevrolet', 'Impala', 'Sedan', 'White', 98765, 7500.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (24578, 2007, 'Jeep', 'Grand Cherokee', 'SUV', 'Gray', 176543, 5800.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (25689, 2006, 'Nissan', 'Murano', 'SUV', 'Red', 154321, 4900.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (26790, 2014, 'Ford', 'Explorer', 'SUV', 'Blue', 87654, 12750.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (27801, 2008, 'Toyota', 'Highlander', 'SUV', 'Silver', 145678, 7800.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (28912, 2010, 'Honda', 'Pilot', 'SUV', 'Green', 123456, 8300.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (29023, 2009, 'Chevrolet', 'Traverse', 'SUV', 'Black', 154321, 6500.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (30134, 2015, 'Mazda', 'CX-9', 'SUV', 'Red', 98765, 14000.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (31245, 2013, 'Hyundai', 'Tucson', 'SUV', 'White', 109876, 8600.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (32356, 2011, 'Ford', 'Flex', 'SUV', 'Blue', 145678, 7200.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (33467, 2007, 'Toyota', '4Runner', 'SUV', 'Silver', 167890, 6700.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (34578, 2012, 'Nissan', 'Pathfinder', 'SUV', 'Gray', 98765, 9000.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (35689, 2014, 'Chevrolet', 'Silverado', 'Truck', 'Red', 87654, 17500.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (36790, 2010, 'Ford', 'F-150', 'Truck', 'Blue', 145678, 11500.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (37801, 2009, 'Dodge', 'Ram 1500', 'Truck', 'Black', 132456, 10750.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (38912, 2006, 'Toyota', 'Tacoma', 'Truck', 'White', 154321, 6900.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (39023, 2008, 'Honda', 'Ridgeline', 'Truck', 'Silver', 145678, 8500.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (40134, 2012, 'GMC', 'Sierra', 'Truck', 'Gray', 98765, 12000.00, 'NO');
+INSERT INTO vehicles(`VIN`, `year`, `make_name`, `model_name`, `vehicle_type`, `vehicle_color`, `mileage`, `vehicle_price`, `SOLD`) VALUES (41245, 2015, 'Ram', '2500', 'Truck', 'Blue', 87654, 19500.00, 'NO');
+
 
 # ---------------------------------------------------------------------- #
 # Add info into "Inventory"                                              #
@@ -121,6 +144,28 @@ INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 72105);
 INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 78901);
 INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 12345);
 INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 89012);
+-- Below was converted using ChatGPT
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 21245);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 22356);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 23467);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 24578);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 25689);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 26790);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 27801);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 28912);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 29023);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 30134);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 31245);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 32356);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 33467);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 34578);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 35689);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 36790);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 37801);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 38912);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 39023);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 40134);
+INSERT INTO inventory(`dealership_id`, `VIN`) VALUES (1, 41245);
 
 # ---------------------------------------------------------------------- #
 # Add info into "Sales_Contracts"                                        #
